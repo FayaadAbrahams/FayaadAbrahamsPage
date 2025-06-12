196cc831f2383a5bb5ae89c2f97112239020e3a7
Fayaad Abrahams <fayaad1999@gmail.com>
2025-06-12 23:46:37 +0200

    Changed up a bunch of styles and made the portfolio look a lot better.
    - Functioning of dark/light mode.
    - Scrolling text container added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
 import './assets/App.css';
 import NavBar from './NavBar.tsx';
-
 function App() {
 
+  const button = document.querySelector('.glow-button') as HTMLElement;
+
+  if (button) {
+    button.addEventListener('mousemove', (e) => {
+      const rect = button.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      button.style.setProperty('--x', `${x}px`);
+      button.style.setProperty('--y', `${y}px`);
+    });
+
+    button.addEventListener('mouseleave', () => {
+      button.style.setProperty('--x', `50%`);
+      button.style.setProperty('--y', `50%`);
+    });
+  }
   return (
     <>
       <NavBar />
       <div className="body-container">
         <div className="body-content-container">
+          <div className="profile-image-container">
+            <img src="https://avatars.githubusercontent.com/u/102885626?v=4" alt="Fayaad Abrahams" />
+          </div>
+          <div className="home-page-demo-container">
+          </div>
           <h1 className='heading-left'>Hey!</h1>
           <h1>I'm Fayaad Abrahams.</h1>
-          <h3>Software Developer </h3>
-          <p>Feel free to explore my projects and get in touch with me.</p>
+          <div className="scrolling-words-container">
+            <h2>I am a, </h2>
+            <div className="scrolling-words-box">
+              <ul>
+                <li>Software Developer.</li>
+                <li>Front-End Developer.</li>
+                <li>Back-End Developer.</li>
+                <li>Team Player.</li>
+                <li>Problem Solver.</li>
+              </ul>
+            </div>
+          </div>
+          <p>Passionate about building clean, efficient, and user-centric applications.</p>
+          <p>Feel free to explore my projects — and don’t hesitate to reach out.</p>
+          <br />
+          <p>Let’s build something <span className='special-glow'>meaningful</span> together.</p>
           <div className="contact-btn-container">
-            <button>Contact Me</button>
-            <button>Resume</button>
+            <button><a href="mailto:fayaad.abrahams1@gmail.com??subject=Fayaad Abrahams Portfolio Inquiry" >Message Me</a></button>
+            <form action="/resume.pdf" target="_blank">
+              <button>Resume</button>
+            </form>
           </div>
         </div>
-      </div>
+      </div >
 
     </>
   )
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,23 +1,43 @@
-import { useState } from 'react';
-import './index.css';
+import { useEffect, useState } from 'react';
+import './index.scss';
 import SunnyIcon from '@mui/icons-material/Sunny';
 import BedtimeIcon from '@mui/icons-material/Bedtime';
 
 const NavBar = () => {
   const [darkModeState, setDarkModeState] = useState(true);
   const [isBouncing, setIsBouncing] = useState(false);
+  const [showingIcon, setShowingIcon] = useState(true);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (darkModeState) {
+      root.classList.remove('light-mode');
+      root.classList.add('dark-mode');
+    } else {
+      root.classList.remove('dark-mode');
+      root.classList.add('light-mode');
+    }
+  }, [darkModeState]);
 
   const toggleDarkMode = () => {
     setIsBouncing(true);
+
     setTimeout(() => {
-      setIsBouncing(false)
+      setShowingIcon(!darkModeState);
+    }, 400);
+
+    setTimeout(() => {
       setDarkModeState(!darkModeState);
+      setShowingIcon(!darkModeState);
+      setIsBouncing(false);
     }, 800);
   };
-
-  var icon = darkModeState ? <SunnyIcon className={`dark-mode-icon ${isBouncing ? 'bounce2' : ''}`} />
-    : <BedtimeIcon className={`dark-mode-icon ${isBouncing ? 'bounce2' : ''}`} />;
-
+  const getIcon = () => {
+    if (isBouncing) {
+      return showingIcon ? <SunnyIcon className="dark-mode-icon" /> : <BedtimeIcon className="dark-mode-icon" />;
+    }
+    return darkModeState ? <SunnyIcon className="dark-mode-icon" /> : <BedtimeIcon className="dark-mode-icon" />;
+  };
   return (
     <>
       < nav className="nav-bar">
@@ -26,7 +46,12 @@ const NavBar = () => {
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a onClick={toggleDarkMode}>{icon}</a></li>
+          <li>
+            <button className="dark-mode-btn" onClick={toggleDarkMode} aria-label='Light/Dark Mode Button' >
+              <div className={isBouncing ? 'animate-bounce' : ''}>
+                {getIcon()}
+              </div>
+            </button></li>
         </ul>
       </nav>
 
