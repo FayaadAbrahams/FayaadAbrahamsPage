759ad26fde84cbc2249414d073b668f44845b06c
Fayaad Abrahams <fayaad1999@gmail.com>
2025-06-15 17:34:31 +0200

    Changed the cards

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,20 @@
 import "../assets/index.scss";
 import "../assets/ContactStyles.css";
 import { FaPhoneAlt, FaLinkedin, FaGithub } from "react-icons/fa";
+import Snackbar from "@mui/material/Snackbar";
+import { useState } from "react";
 const SideBar = () => {
     let iconSize = "35px";
+    const [isOpen, setIsOpen] = useState(false);
+
+    function handleTel() {
+        navigator.clipboard.writeText("0637121904");
+        setIsOpen(!isOpen);
+    }
+
+    function onTelclose() {
+        setIsOpen(false);
+    }
     return (
         <>
             <div className="contact-content-container">
@@ -13,12 +25,18 @@ const SideBar = () => {
                 </div>
                 <div className="contact-info-container">
                     <a href="https://github.com/FayaadAbrahams" target="_blank" aria-label="GitHub Profile" >
-                        <FaGithub size={iconSize} className="github-icon"/>
+                        <FaGithub size={iconSize} className="github-icon" />
                     </a>
                 </div>
                 <div className="contact-info-container">
-                    <a href="tel:0637121904">
-                        <FaPhoneAlt size={iconSize} />
+                    <a href="tel:0637121904" onClick={handleTel}>
+                        <Snackbar
+                            open={isOpen}
+                            autoHideDuration={6000}
+                            onClose={onTelclose}
+                            message="Copied Cellphone Number!"
+                        />
+                        <FaPhoneAlt size={iconSize} className="telephone-icon" />
                     </a>
 
                 </div>
